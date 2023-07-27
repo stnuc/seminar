34470b9f057b1894ece443171e6c38d697d5b92a
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-07-27 12:25:44 +0900

    ps

--- auto_deploy.ps1
+++ auto_deploy.ps1
@@ -1,7 +1,8 @@
 npm run build
-Copy-Item -Path ./build,.git -Destination C:\autodeploy -recurse -Force
+Copy-Item -Path ./build, .git -Destination C:\autodeploy -recurse -Force
 cd C:\autodeploy
 git checkout -b page
+git checkout page
 git add .
 git commit -m "page"
 git push -u origin page --force
