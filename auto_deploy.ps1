27110f10972f38076cb7a7cd20f9655886ee1288
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-08-23 17:02:01 +0900

    feat: auto deploy changed

--- auto_deploy.ps1
+++ auto_deploy.ps1
@@ -1,9 +1,57 @@
-npm run build
-git checkout -t origin/page
-git checkout page
-git pull origin page
-Copy-Item -Path ./build/* -Destination ./ -recurse -Force
-git add .
-git commit -m "page"
-git push -u origin page
-git checkout main
+Set-PSDebug -Off;
+
+$PROGRAMNAME = "Auto Deploy"
+$TargetBranch = "page"
+
+$ErrorActionPreference = "Stop"
+
+& cmd /c 'npm run build'
+if ($LASTEXITCODE -ne 0) {
+    "${PROGRAMNAME}: npm run build failed, Error code: ${LASTEXITCODE}"
+    exit $LASTEXITCODE
+}
+
+& cmd /c "git checkout -t origin/${TargetBranch}"
+if ($LASTEXITCODE -eq 0) {
+    "${PROGRAMNAME}: pull branch from origin/${TargetBranch} success"
+    & cmd /c "git pull origin ${TargetBranch}"
+    Copy-Item -Path ./build/* -Destination ./ -recurse -Force
+    & cmd /c "git add ."
+    & cmd /c "git commit -m '${TargetBranch}'"
+    & cmd /c "git push origin ${TargetBranch}"
+    & cmd /c "git checkout main"
+}
+else {
+    "${PROGRAMNAME}: pull branch from origin/${TargetBranch} failed, ErrorCode: ${LASTEXITCODE}"
+}
+
+& cmd /c "git checkout ${TargetBranch}"
+if ($LASTEXITCODE -eq 0) {
+    "${PROGRAMNAME}: checkout to ${TargetBranch} branch success"
+    & cmd /c "git pull origin ${TargetBranch}"
+    Copy-Item -Path ./build/* -Destination ./ -recurse -Force
+    & cmd /c "git add ."
+    & cmd /c "git commit -m '${TargetBranch}'"
+    & cmd /c "git push origin -u ${TargetBranch}"
+    & cmd /c "git checkout main"
+}
+else {
+    "${PROGRAMNAME}: checkout to ${TargetBranch} branch failed, ErrorCode: ${LASTEXITCODE}"
+}
+
+& cmd /c "git checkout --orphan ${TargetBranch}"
+if ($LASTEXITCODE -eq 0) {
+    "${PROGRAMNAME}: create orphan branch success"
+    & cmd /c "git rm --cached -r ."
+    Remove-Item -recurse ./* -exclude build, .git/, node_modules, .gitignore
+    Copy-Item -Path ./build/* -Destination ./ -recurse -Force
+    & cmd /c "git add ."
+    & cmd /c "git commit -m '${TargetBranch}'"
+    & cmd /c "git push origin -u ${TargetBranch}"
+    & cmd /c "git checkout main"
+}
+else {
+    "${PROGRAMNAME}: create orphan branch failed"
+    "${PROGRAMNAME}: auto deploy is failed"
+    exit $LASTEXITCODE
+}
